1c8e333f5cad8fd8642ecb109c9710fa69f610ab
vksingh9 <105032849+vishal080@users.noreply.github.com>
2023-12-12 13:05:33 +0530

    second  part done

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/vishal080/main.go
+
+go 1.21.5
+
+require (
+	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/gorilla/mux v1.8.1
+	github.com/mattn/go-sqlite3 v1.14.18
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+type Welcome struct {
+	Sale string
+	Time string
+}
+
+func main() {
+	welcome := Welcome{"Sale Begins Now", time.Now().Format(time.Stamp)}
+	template := template.Must(template.ParseFiles("template/template.html"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if sale := r.FormValue("sale"); sale != "" {
+			welcome.Sale = sale
+		}
+		if err := template.ExecuteTemplate(w, "template.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+	fmt.Println(http.ListenAndServe(":8000", nil))
+}
