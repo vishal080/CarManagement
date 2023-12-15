c5618f72f6e8fd2d131e689089bfe97078f7db30
vksingh9 <105032849+vishal080@users.noreply.github.com>
2023-12-15 10:26:25 +0530

    sql-lite  done for backend first step

--- main.go
+++ main.go
@@ -1,92 +1,185 @@
+// package main
+
+// import (
+//     "context"
+//     "fmt"
+//     "html/template"
+//     "net/http"
+//     "time"
+
+//     "go.mongodb.org/mongo-driver/mongo"
+//     "go.mongodb.org/mongo-driver/mongo/options"
+// )
+
+// type Car struct {
+//     ID        int
+//     Make      string
+//     Model     string
+//     Status    string
+//     CreatedAt time.Time
+// }
+
+// type Welcome struct {
+//     Sale string
+//     Time string
+//     Cars []Car // Store cars in a slice
+// }
+
+// var cars []Car // In-memory storage for cars
+
+// func main() {
+//     // MongoDB connection
+//     client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+//     if err != nil {
+//         panic(err.Error())
+//     }
+//     defer func() {
+//         if err = client.Disconnect(context.Background()); err != nil {
+//             panic(err.Error())
+//         }
+//     }()
+
+//     // Ping the MongoDB server
+//     err = client.Ping(context.Background(), nil)
+//     if err != nil {
+//         panic(err.Error())
+//     }
+
+//     fmt.Println("Connected to MongoDB!")
+
+//     // Go HTTP server setup
+//     welcome := Welcome{"Sale Begins Now", time.Now().Format(time.Stamp), cars}
+//     template := template.Must(template.ParseFiles("template/template.html"))
+	
+
+//     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+//     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+//         if sale := r.FormValue("sale"); sale != "" {
+//             welcome.Sale = sale
+//         }
+//         welcome.Cars = cars // Update cars data in welcome struct
+//         if err := template.ExecuteTemplate(w, "template.html", welcome); err != nil {
+//             http.Error(w, err.Error(), http.StatusInternalServerError)
+//         }
+//     })
+
+//     http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+//         if r.Method == http.MethodPost {
+//             r.ParseForm()
+//             make := r.FormValue("make")
+//             model := r.FormValue("model")
+
+//             newID := len(cars) + 1
+
+//             newCar := Car{newID, make, model, "Available", time.Now()}
+//             cars = append(cars, newCar)
+
+//             // Insert the newly added car to MongoDB
+//             collection := client.Database("carDB").Collection("cars")
+//             _, err := collection.InsertOne(context.Background(), newCar)
+//             if err != nil {
+//                 panic(err.Error())
+//             }
+
+//             http.Redirect(w, r, "/", http.StatusFound)
+//             return
+//         }
+//         http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+//     })
+
+//     fmt.Println(http.ListenAndServe(":8000", nil))
+// }
 package main
 
 import (
-    "context"
-    "fmt"
-    "html/template"
-    "net/http"
-    "time"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Car struct {
-    ID        int
-    Make      string
-    Model     string
-    Status    string
-    CreatedAt time.Time
+	ID    int
+	Make  string
+	Model string
 }
 
-type Welcome struct {
-    Sale string
-    Time string
-    Cars []Car // Store cars in a slice
-}
-
-var cars []Car // In-memory storage for cars
+var db *sql.DB
 
 func main() {
-    // MongoDB connection
-    client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
-    if err != nil {
-        panic(err.Error())
-    }
-    defer func() {
-        if err = client.Disconnect(context.Background()); err != nil {
-            panic(err.Error())
-        }
-    }()
+	// Connect to SQLite3 database
+	var err error
+	db, err = sql.Open("sqlite3", "./cars.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-    // Ping the MongoDB server
-    err = client.Ping(context.Background(), nil)
-    if err != nil {
-        panic(err.Error())
-    }
+	// Create cars table if it doesn't exist
+	createTable()
 
-    fmt.Println("Connected to MongoDB!")
+	http.HandleFunc("/addCar", addCarHandler)
+	http.HandleFunc("/updateCar", updateCarHandler)
+	http.HandleFunc("/deleteCar", deleteCarHandler)
+	http.HandleFunc("/", indexHandler)
 
-    // Go HTTP server setup
-    welcome := Welcome{"Sale Begins Now", time.Now().Format(time.Stamp), cars}
-    template := template.Must(template.ParseFiles("template/template.html"))
-	
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
 
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+func createTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS cars (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		make TEXT,
+		model TEXT
+	);
+	`
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        if sale := r.FormValue("sale"); sale != "" {
-            welcome.Sale = sale
-        }
-        welcome.Cars = cars // Update cars data in welcome struct
-        if err := template.ExecuteTemplate(w, "template.html", welcome); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-    })
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-    http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
-        if r.Method == http.MethodPost {
-            r.ParseForm()
-            make := r.FormValue("make")
-            model := r.FormValue("model")
+func addCarHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	make := r.Form.Get("make")
+	model := r.Form.Get("model")
 
-            newID := len(cars) + 1
+	_, err := db.Exec("INSERT INTO cars (make, model) VALUES (?, ?)", make, model)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-            newCar := Car{newID, make, model, "Available", time.Now()}
-            cars = append(cars, newCar)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
 
-            // Insert the newly added car to MongoDB
-            collection := client.Database("carDB").Collection("cars")
-            _, err := collection.InsertOne(context.Background(), newCar)
-            if err != nil {
-                panic(err.Error())
-            }
+func updateCarHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle update logic here
+	// ...
+}
 
-            http.Redirect(w, r, "/", http.StatusFound)
-            return
-        }
-        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-    })
+func deleteCarHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := r.FormValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Println(http.ListenAndServe(":8000", nil))
+	_, err = db.Exec("DELETE FROM cars WHERE id = ?", id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// Serve the HTML file
+	http.ServeFile(w, r, "template/template.html")
 }
