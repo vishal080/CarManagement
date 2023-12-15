ad825d3eac40dd70088231a61fd43fcc48c73ac0
vksingh9 <105032849+vishal080@users.noreply.github.com>
2023-12-15 15:32:15 +0530

    today part done

--- main.go
+++ main.go
@@ -1,95 +1,3 @@
-// package main
-
-// import (
-//     "context"
-//     "fmt"
-//     "html/template"
-//     "net/http"
-//     "time"
-
-//     "go.mongodb.org/mongo-driver/mongo"
-//     "go.mongodb.org/mongo-driver/mongo/options"
-// )
-
-// type Car struct {
-//     ID        int
-//     Make      string
-//     Model     string
-//     Status    string
-//     CreatedAt time.Time
-// }
-
-// type Welcome struct {
-//     Sale string
-//     Time string
-//     Cars []Car // Store cars in a slice
-// }
-
-// var cars []Car // In-memory storage for cars
-
-// func main() {
-//     // MongoDB connection
-//     client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
-//     if err != nil {
-//         panic(err.Error())
-//     }
-//     defer func() {
-//         if err = client.Disconnect(context.Background()); err != nil {
-//             panic(err.Error())
-//         }
-//     }()
-
-//     // Ping the MongoDB server
-//     err = client.Ping(context.Background(), nil)
-//     if err != nil {
-//         panic(err.Error())
-//     }
-
-//     fmt.Println("Connected to MongoDB!")
-
-//     // Go HTTP server setup
-//     welcome := Welcome{"Sale Begins Now", time.Now().Format(time.Stamp), cars}
-//     template := template.Must(template.ParseFiles("template/template.html"))
-	
-
-//     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
-//     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-//         if sale := r.FormValue("sale"); sale != "" {
-//             welcome.Sale = sale
-//         }
-//         welcome.Cars = cars // Update cars data in welcome struct
-//         if err := template.ExecuteTemplate(w, "template.html", welcome); err != nil {
-//             http.Error(w, err.Error(), http.StatusInternalServerError)
-//         }
-//     })
-
-//     http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
-//         if r.Method == http.MethodPost {
-//             r.ParseForm()
-//             make := r.FormValue("make")
-//             model := r.FormValue("model")
-
-//             newID := len(cars) + 1
-
-//             newCar := Car{newID, make, model, "Available", time.Now()}
-//             cars = append(cars, newCar)
-
-//             // Insert the newly added car to MongoDB
-//             collection := client.Database("carDB").Collection("cars")
-//             _, err := collection.InsertOne(context.Background(), newCar)
-//             if err != nil {
-//                 panic(err.Error())
-//             }
-
-//             http.Redirect(w, r, "/", http.StatusFound)
-//             return
-//         }
-//         http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-//     })
-
-//     fmt.Println(http.ListenAndServe(":8000", nil))
-// }
 package main
 
 import (
@@ -160,8 +68,22 @@ func addCarHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateCarHandler(w http.ResponseWriter, r *http.Request) {
-	// Handle update logic here
-	// ...
+	r.ParseForm()
+	idStr := r.Form.Get("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	make := r.Form.Get("make")
+	model := r.Form.Get("model")
+
+	_, err = db.Exec("UPDATE cars SET make = ?, model = ? WHERE id = ?", make, model, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func deleteCarHandler(w http.ResponseWriter, r *http.Request) {
